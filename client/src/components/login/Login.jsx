de7e19a4992b9898c06187fb67e7d3575b4e92bc
Prassad Raj R <prassadpr10@gmail.com>
2024-04-11 21:37:25 +0530

    chg

--- client/src/components/login/Login.jsx
+++ client/src/components/login/Login.jsx
@@ -1,26 +1,44 @@
-import React from "react";
+import React, { useState } from "react";
 import "./login.css";
 
 function Login() {
+  const [avatar, setAvatar] = useState({
+    file: null,
+    url: "",
+  });
+  const handleClick = (e) => {
+    if (e.target.files[0]) {
+      setAvatar({
+        file: e.target.files[0],
+        url: URL.createObjectURL(e.target.files[0]),
+      });
+    }
+  };
   return (
     <div className="login">
       <div className="item">
         <h2>Welcome back,</h2>
         <form>
-          <input type="text" placeholder="Email" name="email"/>
-          <input type="password" placeholder="Password" name="password"/>
+          <input type="text" placeholder="Email" name="email" />
+          <input type="password" placeholder="Password" name="password" />
           <button>Sign In</button>
         </form>
       </div>
       <div className="separate"></div>
       <div className="item">
-      <form>
-        <label htmlFor="file">
-            <img src="" alt="" /> Upload an Image</label>
-        <input type="file" id="file" style={{display:"none"}} />
-          <input type="text" placeholder="UserName" name="username"/>
-          <input type="email" placeholder="Email" name="email"/>
-          <input type="password" placeholder="Password" name="password"/>
+        <form>
+          <label htmlFor="file">
+            <img src={avatar.url||'../avatar.png'} alt="" /> Upload an Image
+          </label>
+          <input
+            type="file"
+            id="file"
+            style={{ display: "none" }}
+            onChange={handleClick}
+          />
+          <input type="text" placeholder="Username" name="username" />
+          <input type="email" placeholder="Email" name="email" />
+          <input type="password" placeholder="Password" name="password" />
           <button>Sign Up</button>
         </form>
       </div>
