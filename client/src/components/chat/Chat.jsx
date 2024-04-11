c2d50a2eb5f18bfba45a646f712e045fb98fa6dd
Prassad Raj R <prassadpr10@gmail.com>
2024-04-11 17:13:01 +0530

    chg

--- client/src/components/chat/Chat.jsx
+++ client/src/components/chat/Chat.jsx
@@ -1,9 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
 import "./chat.css";
+import EmojiPicker from "emoji-picker-react";
 
 function Chat() {
+  const [openEmoji, SetOpenEmoji] = useState(false);
+  const [text, SetText] = useState("");
+  const handleEmoji = (e) => {
+    SetText((prev) => prev + e.emoji);
+    SetOpenEmoji(false);
+  };
+
   return (
     <div className="chat">
+      {/* top */}
+
       <div className="top">
         <div className="user">
           <img src="./avatar.png" alt="" />
@@ -18,10 +28,97 @@ function Chat() {
           <img src="./info.png" alt="" />
         </div>
       </div>
-      <div className="center"></div>
+      {/* center */}
+
+      <div className="center">
+        <div className="message own">
+          <div className="texts">
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quam
+              voluptatum possimus quo, quas magnam vitae maiores, quae voluptas
+              aperiam repellendus quidem error. Accusamus blanditiis eum error,
+              architecto ratione animi odio.
+            </p>
+            <span>1min ago</span>
+          </div>
+        </div>
+        <div className="message">
+          <img src="./avatar.png" alt="" />
+          <div className="texts">
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quam
+              voluptatum possimus quo, quas magnam vitae maiores, quae voluptas
+              aperiam repellendus quidem error. Accusamus blanditiis eum error,
+              architecto ratione animi odio.
+            </p>
+            <span>1min ago</span>
+          </div>
+        </div>
+        <div className="message own">
+          <div className="texts">
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quam
+              voluptatum possimus quo, quas magnam vitae maiores, quae voluptas
+              aperiam repellendus quidem error. Accusamus blanditiis eum error,
+              architecto ratione animi odio.
+            </p>
+            <span>1min ago</span>
+          </div>
+        </div>
+        <div className="message">
+          <img src="./avatar.png" alt="" />
+          <div className="texts">
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quam
+              voluptatum possimus quo, quas magnam vitae maiores, quae voluptas
+              aperiam repellendus quidem error. Accusamus blanditiis eum error,
+              architecto ratione animi odio.
+            </p>
+            <span>1min ago</span>
+          </div>
+        </div>
+        <div className="message own">
+          <img src="./avatar.png" alt="" />
+          <div className="texts">
+            <img
+              src="https://www.sportico.com/wp-content/uploads/2021/06/CristianoRonaldoJuventus_AP20287595970355-e1622763628281.jpg?w=1280&h=790&crop=1"
+              alt=""
+            />
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quam
+              voluptatum possimus quo, quas magnam vitae maiores, quae voluptas
+              aperiam repellendus quidem error. Accusamus blanditiis eum error,
+              architecto ratione animi odio.
+            </p>
+            <span>1min ago</span>
+          </div>
+        </div>
+      </div>
+
+      {/* bottom */}
       <div className="bottom">
-        <div className="icons"></div>
-        <input type="text"  placeholder="Typing..."/>
+        <div className="icons">
+          <img src="./img.png" alt="" />
+          <img src="./camera.png" alt="" />
+          <img src="./mic.png" alt="" />
+        </div>
+        <input
+          type="text"
+          onChange={(e) => SetText(e.target.value)}
+          placeholder="Typing..."
+          value={text}
+        />
+        <div className="emoji">
+          <img
+            src="./emoji.png"
+            onClick={() => SetOpenEmoji((prev) => !prev)}
+            alt=""
+          />
+          <div className="picker">
+            <EmojiPicker open={openEmoji} onEmojiClick={handleEmoji} />
+          </div>
+        </div>
+        <button className="sendButton">Send</button>
       </div>
     </div>
   );
